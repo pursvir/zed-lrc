e354202d634b5b706ee2acc506009d035ba69256
pursvir <i-have@no.email>
2026-09-24 01:43:25 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# zed-lrc
+
+[Song Lyrics](https://en.wikipedia.org/wiki/LRC_(file_format)) (.lrc) file format support for [Zed](https://zed.dev/).
---
+++ languages/lrc/highlights.scm
@@ -0,0 +1,20 @@
+[
+  (title_key)
+  (artist_key)
+  (length_key)
+  (author_key)
+  (tool_key)
+  (album_key)
+  (offset_key)
+  (by_key)
+  (lyricist_key)
+] @boolean
+
+[
+  (value)
+  (length_value)
+  (offset_value)
+] @string
+
+(timestamp) @property
+(comment) @comment
